a5e34f6ef7c89b7ac2db2208ad9a5f31a8e77d7e
s_iceman <s_iceman@protonmail.com>
2022-05-22 11:41:28 +0300

    fix: modify logic to remove the directory if it exists

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,33 +1,34 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs/promises');
 
 const SRC = path.join(__dirname, 'files');
 const COPY = path.join(__dirname, 'files-copy');
 
-function copyDir() {
+async function rmDir(dir) {
+  const entries = await fs.readdir(dir, {withFileTypes: true});
+  await Promise.all(entries.map(entry => {
+    const fullPath = path.join(dir, entry.name);
+    return entry.isDirectory() ? rmDir(fullPath) : fs.unlink(fullPath);
+  }));
+  await fs.rmdir(dir);
+}
+
+async function copyDir() {
+  const isDirExists = !!(await fs.stat(COPY).catch(e => false));
+  if (isDirExists) {
+    await rmDir(COPY);
+  }
+
   try {
-    fs.mkdir(COPY, {recursive: true}, () => {});
+    await fs.mkdir(COPY, {recursive: true}, () => {});
+    const files = await fs.readdir(SRC);
+    for (const file of files) {
+      fs.copyFile(path.join(SRC, file), path.join(COPY, file));
+    }
   } catch (err) {
     console.error(err.message);
     return;
   }
-
-  fs.readdir(
-    SRC, (err, files) => {
-      if (err) {
-        console.error(err.message);
-        return;
-      } else {
-        for (const file of files) {
-          fs.copyFile(
-            path.join(SRC, file),
-            path.join(COPY, file),
-            () => {}
-          );
-        }
-      }
-    }
-  );
 }
 
 copyDir();
